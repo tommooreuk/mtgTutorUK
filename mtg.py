3b655fd96af5b9305693d81d770feb5ce15b0b33
Kyle <Kyle>
2016-10-07 20:01:03 +0100

    Added 3rd site, finds specific card now

--- mtg.py
+++ mtg.py
@@ -8,16 +8,18 @@ def magicmadhouse(card):
     a = card.replace(' ', '-').lower()
     c = "".join(z for z in a if z not in ('!','.',':',",","'"))
 
-    r  = requests.get("https://www.magicmadhouse.co.uk/search/magic-the-gathering-c1/singles-english-t85/" + c)
+    r  = requests.get("https://www.magicmadhouse.co.uk/search/magic-the-gathering-c1/" + c)
     data = r.text
     soup = BeautifulSoup(data,"html.parser")
 
     test = soup.findAll("div", { "class" : "product_details" })
 
-    print len(test), "MATCHES FOUND FOR", card.upper()
-
     for n in range(len(test)):
-        print str(test[n].find("span",{ "class" : "GBP" }))[20:-7], ",", str(test[n].find("div",{ "class" : "product_stock_status" }))[35:-6]
+        x = str(test[n].find({ "a" : "product_title"})).lower()[:-4]
+        for k in range(3):
+            x = x[x.index(">")+2:]
+        if (x == card.lower() or x[:len(card)+2] == card.lower() + " ("):
+            print x, ",", str(test[n].find("span",{ "class" : "GBP" }))[20:-7], ",", str(test[n].find("div",{ "class" : "product_stock_status" }))[35:-6]
 
 def magicsingles(card):
     a = card.replace(',','%2C').replace(' ','+').lower()
@@ -28,19 +30,41 @@ def magicsingles(card):
 
     test = soup.findAll("div", { "class" : "prod-info" })
 
-    print len(test), "MATCHES FOUND FOR", card.upper()
-    
     for n in range(len(test)):
         x = str(test[n].findAll("span", {"class" : "price-num"})[1])
         y = str(test[n].find("span",{ "class" : "qty-in-stock"}))
+        z = str(test[n].find("a",{ "class" : "product-title"}))
+        z = z[z.index(">")+1:-4].lower()
         if y != "None":
             y = y[y.index(">")+39:-40]
         if y[0] == "Â":
             y = "1" + y[1:]
-        print "£" + x[x.index(">")+1:-7] + " , " + y.replace("Â","")
+        if(z == card.lower() or z[:len(card)+2] == card.lower() + " ("):
+            print z, ",", "£" + x[x.index(">")+1:-7] , ",", y.replace("Â","")
 
-    
+def trolltrader(card):
+    a = card.replace(',','%2C').replace(' ','+').lower()
+
+    r  = requests.get("http://www.trolltradercards.com/products/search?q=" + a)
+    data = r.text
+    soup = BeautifulSoup(data,"html.parser")
+
+    test = soup.findAll("li", { "class" : "product"})
+
+    for n in range(len(test)):
+        x = str(test[n].find("span", {"class" : "price"}, {"itemprop" : "price"}))
+        y = str(test[n].find("span",{ "class" : "qty"}))
+        z = str(test[n].find("h4", {"class" :"name"}))
+        z = z[z.index(">")+1:-5].lower()
+        if y != "None":
+            y = "".join(y[y.index(">")+1:-7].split())
+        else:
+            y = "0instock"
+        if(z == card.lower() or z[:len(card)+2] == card.lower() + " -"):           
+            print z, ",", "".join(x[x.index(">")+1:-7].split())[1:], ",", y[:-7], "in stock"
+            
 a = raw_input("Enter a card name: ")
+
 
 print "\nMagicSingles:\n"   
 magicsingles(a)
@@ -48,5 +72,7 @@ magicsingles(a)
 print "\nMagicMadhouse:\n"
 magicmadhouse(a)
 
+print "\nTrollTraders:\n"
+trolltrader(a)
 
 
