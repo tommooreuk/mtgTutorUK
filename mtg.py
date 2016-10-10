d2b89e8e9ea9128e8c6bcf10252273516bceed74
Kyle <Kyle>
2016-10-10 21:49:52 +0100

    added two more sites, will deploy site soon

--- mtg.py
+++ mtg.py
@@ -8,7 +8,7 @@ def magicmadhouse(card):
     a = card.replace(' ', '-').lower()
     c = "".join(z for z in a if z not in ('!','.',':',",","'"))
 
-    r  = requests.get("https://www.magicmadhouse.co.uk/search/magic-the-gathering-c1/" + c)
+    r  = requests.get("http://www.magicmadhouse.co.uk/search/magic-the-gathering-c1/" + c)
     data = r.text
     soup = BeautifulSoup(data,"html.parser")
 
@@ -39,7 +39,7 @@ def magicsingles(card):
             y = y[y.index(">")+39:-40]
         if y[0] == "Â":
             y = "1" + y[1:]
-        if(z == card.lower() or z[:len(card)+2] == card.lower() + " ("):
+        if(z == card.lower() or z[:len(card)+2] == card.lower() + " (" or z[:len(card)+2] == card.lower() + " /"):
             print z, ",", "£" + x[x.index(">")+1:-7] , ",", y.replace("Â","")
 
 def trolltrader(card):
@@ -60,19 +60,55 @@ def trolltrader(card):
             y = "".join(y[y.index(">")+1:-7].split())
         else:
             y = "0instock"
-        if(z == card.lower() or z[:len(card)+2] == card.lower() + " -"):           
+        if(z == card.lower() or z[:len(card)+2] == card.lower() + " -" or z[:len(card)+2] == card.lower() + " /"):           
             print z, ",", "".join(x[x.index(">")+1:-7].split())[1:], ",", y[:-7], "in stock"
-            
+
+def manaleak(card):
+    a = card.replace(',','%2C').replace(' ','+').lower()
+
+    r  = requests.get("http://www.manaleak.com/magic-the-gathering/advanced_search_result.php?keywords=" + a)
+    data = r.text
+    soup = BeautifulSoup(data,"html.parser")    
+
+    test = soup.findAll("div", { "class" : "product_info_wrapper"})
+    test = test[:-8]
+
+    for n in range(len(test)):
+        x = str(test[n].find("span", {"class" : "productSpecialPrice"}))[35:-7]
+        y = str(test[n].find("button", {"class" : "pull-left qty_change qty_minus"}))[61]
+        z = str(test[n].find("h3", {"class" : "product_name_wrapper name equal-height_listing_name"}))[70:-16]
+        z = z[z.index(">")+1:].lower()
+
+        if(z == card.lower() or z[:len(card)+2] == card.lower() + " -" or z[:len(card)+2] == card.lower() + " /" or z[:len(card)+2] == card.lower() + " ("): 
+            print z, ",", x,",", y, "in stock"
+    
+    
 a = raw_input("Enter a card name: ")
 
+def magiccardtrader(card):
+    a = card.replace(',','%2C').replace(' ','+').lower()
 
+    r  = requests.get("http://www.themagiccardtrader.com/products/search?query=" + a + "&x=0&y=0")
+    data = r.text
+    soup = BeautifulSoup(data,"html.parser")
+
+    test = soup.findAll("tr", { "class" : "product_row"})
+
+    for n in range(len(test)):
+        print str(test[n].find("td", {"class" : "qty"}))
+        print str(test[n].find("a href"))
+    
 print "\nMagicSingles:\n"   
-magicsingles(a)
-
-print "\nMagicMadhouse:\n"
-magicmadhouse(a)
+#magicsingles(a)
 
 print "\nTrollTraders:\n"
-trolltrader(a)
+#trolltrader(a)
 
+print "\nManaLeak:\n"
+#manaleak(a)
 
+print "\nMagicMadhouse:\n"
+#magicmadhouse(a)
+
+print "\nTheMagicCardTrader:\n"
+magiccardtrader(a)
